613a8cee6295e8572516039be30d5f0573b0009b
bd6981 <brittanyjdavis22@gmail.com>
2023-01-17 22:56:47 -0500

    search query added

--- index.js
+++ index.js
@@ -25,4 +25,42 @@ app.get('/products/:productId', async (req, res) => {
         }
     });
 // first route done --------^
+//Get product reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+    const { productId } = req.params;
+    
+    try {
+        const response = await request(`${baseUrl}&url=http://www.amazon.com/product-reviews/${productId}`);
+        res.json(JSON.parse(response))
+    } catch (error) {
+        res.json(error)
+        }
+    });
+// second route done --------^
+//Get product offers
+app.get('/products/:productId/offers/', async (req, res) => {
+    const { productId } = req.params;
+    
+    try {
+        const response = await request(`${baseUrl}&url=http://www.amazon.com/gp/offer-/${productId}`);
+        res.json(JSON.parse(response))
+    } catch (error) {
+        res.json(error)
+        }
+    });
+// third route done --------^
+//Get search Query 
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=http://www.amazon.com/s?k=${searchQuery}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+// fourth route done --------^
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
